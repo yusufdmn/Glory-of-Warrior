e978b8ec365e00258e5c86954debc8e5b8234c0f
Yusuf Duman <yusufdumans61@gmail.com>
2024-08-03 17:24:29 +0300

    Add market model

---
+++ Glory of Warrior/Assets/Scripts/Inventory System/MarketModel.cs
@@ -0,0 +1,20 @@
+using Inventory_System.ScriptableObjects;
+
+namespace Inventory_System
+{
+    public class MarketModel
+    {
+        public Item[] MarketItems { get; set; }
+        
+        public delegate void OnItemBoughtDelegate(Item item);
+        public event OnItemBoughtDelegate OnItemBought;
+
+        public void BuyItem(Item item ,int currentCoin)
+        {
+            if (item.Price > currentCoin)
+                return;
+            
+            OnItemBought?.Invoke(item);
+        }
+    }
+}
