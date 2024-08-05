2ecdc928abfc8457ac80b4ad6822b96bd5d2a528
Yusuf Duman <yusufdumans61@gmail.com>
2024-08-05 21:54:30 +0300

    Create Inventory System Initializer

---
+++ Glory of Warrior/Assets/Scripts/Common/Interfaces/SystemInitializer.cs
@@ -0,0 +1,7 @@
+namespace Common.Interfaces
+{
+    public interface SystemInitializer
+    {
+        public void InitializeSystem();
+    }
+}
---
+++ Glory of Warrior/Assets/Scripts/Inventory System/InventorySystemInitializer.cs
@@ -0,0 +1,28 @@
+using System;
+using Common.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Inventory_System
+{
+    public class InventorySystemInitializer: MonoBehaviour, SystemInitializer
+    {
+        [Inject] private InventoryController _inventoryController;
+        [Inject] private InventoryModel _inventoryModel;
+        [Inject] private MarketModel _marketModel;
+        [Inject] private MarketView _marketView;
+        [Inject] private InventoryView _inventoryView;
+        [Inject] private ItemStorage _itemStorage;
+        
+        public void InitializeSystem()
+        {
+            _marketModel.MarketItems = _itemStorage.AllItems;
+            _inventoryController.Initialize(_inventoryModel, _inventoryView, _marketModel, _marketView);
+        }
+
+        private void Awake()
+        {
+            InitializeSystem();
+        }
+    }
+}
