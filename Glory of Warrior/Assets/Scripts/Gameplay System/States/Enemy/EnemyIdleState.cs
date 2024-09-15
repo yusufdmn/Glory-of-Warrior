32d087e1ea4f0662e1b90ee0d4c22f2109609ac0
Yusuf Duman <yusufdumans61@gmail.com>
2024-09-15 20:11:44 +0300

    Add Idle State for Enemy

---
+++ Glory of Warrior/Assets/Scripts/Gameplay System/States/Enemy/EnemyIdleState.cs
@@ -0,0 +1,32 @@
+using Gameplay_System.Model;
+using UnityEngine;
+
+namespace Gameplay_System.States.Enemy
+{
+    public class EnemyIdleState:IState
+    {
+        private EnemyModel _enemyModel;
+        private EnemyAnimationManager _animationManager;
+        
+        public void Initialize(EnemyModel enemyModel)
+        {
+            _enemyModel = enemyModel;
+            _animationManager = enemyModel.AnimationManager;
+        }
+        
+        public void OnEnter()
+        {
+            _animationManager.StopRun();
+        }
+
+        public void UpdateState()
+        {
+            _enemyModel.WatchTarget();
+        }
+
+        public void OnExit()
+        {
+            _animationManager.StartRun();
+        }
+    }
+}
