8b49422098966ce88f827fadec026b94c4b46134
Yusuf Duman <yusufdumans61@gmail.com>
2024-10-08 22:02:23 +0300

    Bug fix - custom billboard

--- Glory of Warrior/Assets/Scripts/Helper/CustomBillboard.cs
+++ Glory of Warrior/Assets/Scripts/Helper/CustomBillboard.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Helper
 {
@@ -9,6 +10,7 @@ namespace Helper
         private void Start()
         {
             _camera = Camera.main;
+            SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
         private void LateUpdate()
@@ -21,6 +23,16 @@ namespace Helper
             Vector3 direction = _camera.transform.position - transform.position;
             Quaternion rotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Euler(-rotation.eulerAngles.x, 0, 0);
+        }
+        
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            _camera = Camera.main;  // Update the camera reference with the main camera in the new scene
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 }
