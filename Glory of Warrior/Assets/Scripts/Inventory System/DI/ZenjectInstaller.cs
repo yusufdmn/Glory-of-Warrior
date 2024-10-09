6265d246c0ac2f3f170f10cf705b425fadff9712
Yusuf Duman <yusufdumans61@gmail.com>
2024-10-09 13:15:06 +0300

    Create Zenject Installer for Inventory

---
+++ Glory of Warrior/Assets/Scripts/Inventory System/DI/ZenjectInstaller.cs
@@ -0,0 +1,22 @@
+using Health_System;
+using Inventory_System.Controller;
+using Inventory_System.Model;
+using Inventory_System.View;
+using Zenject;
+
+namespace Inventory_System.DI
+{
+    public class ZenjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ItemStorage>().FromComponentInHierarchy().AsTransient();
+            Container.Bind<MarketModel>().AsSingle();
+            Container.Bind<InventoryModel>().AsSingle();
+            Container.Bind<InventoryController>().AsSingle();
+            Container.Bind<InventoryView>().FromComponentInHierarchy().AsSingle();
+            Container.Bind<MarketView>().FromComponentInHierarchy().AsSingle();
+            
+        }
+    }
+}
