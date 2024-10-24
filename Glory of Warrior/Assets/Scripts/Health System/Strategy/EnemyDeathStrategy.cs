1048a757571a45cefe696b1c354e9f3db9b68f8b
Yusuf Duman <yusufdumans61@gmail.com>
2024-10-24 23:29:13 +0300

    Update enemy death strategy - add logic

--- Glory of Warrior/Assets/Scripts/Health System/Strategy/EnemyDeathStrategy.cs
+++ Glory of Warrior/Assets/Scripts/Health System/Strategy/EnemyDeathStrategy.cs
@@ -1,9 +1,15 @@
+using Gameplay_System.Gameplay_Management;
+using Zenject;
+
 namespace Health_System.Strategy
 {
     public class EnemyDeathStrategy: IDeathStrategy
     {
+        [Inject] private GameplayManager _gameplayManager;
+        
         public void Execute()
         {
+            _gameplayManager.OnEnemyDied();
             //Enemy Died. Update UI.
         }
     }
