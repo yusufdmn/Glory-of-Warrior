1d40858283e360e545465a39fd5d65f3996dd3b2
Yusuf Duman <yusufdumans61@gmail.com>
2025-01-02 21:01:44 +0300

    bug fix - deactivate after die animation correctly

--- Glory of Warrior/Assets/Scripts/Gameplay System/Helper/OnDeathEventBehaviour.cs
+++ Glory of Warrior/Assets/Scripts/Gameplay System/Helper/OnDeathEventBehaviour.cs
@@ -4,10 +4,19 @@ namespace Gameplay_System.Helper
 {
     public class OnDeathEventBehaviour : StateMachineBehaviour
     {
-        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        // public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        // {
+        //     GameObject character = animator.gameObject;
+        //     character.SetActive(false);
+        // }
+        
+        public override void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            GameObject character = animator.gameObject;
-            character.SetActive(false);
+            // set gameobject of the animator to inactive when the current state came to the end by checking its time.
+            if (stateInfo.normalizedTime >= 0.95f)
+            { 
+                animator.gameObject.SetActive(false);;
+            }
         }
     }
 }
